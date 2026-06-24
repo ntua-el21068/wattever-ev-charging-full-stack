3bb6b11f2d7a2f37808c0c4235768dea95a78268
karolinaskg <gregocham4@gmail.com>
2026-06-24 23:36:17 +0300

    Connect fronted to live API and add vercel rewrite rules

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -3,6 +3,6 @@
 // FOR GIT (Coworkers): Keep this as localhost!
 // FOR PHONE TESTING: Change this to your IP (e.g., http://192.168.1.10:9876), but DON'T commit the IP.
 
-const API_BASE_URL = "https://localhost:9876"; 
+const API_BASE_URL = "[https://wattever-api.onrender.com/api](https://wattever-api.onrender.com/api)"; 
 
 export default API_BASE_URL;
